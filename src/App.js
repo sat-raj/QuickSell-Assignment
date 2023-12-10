08f4f9ef482b82eab925b3bc6551c6880b4ae73b
Satyam Raj <73134180+sat-raj@users.noreply.github.com>
2023-12-10 09:14:47 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// App.js
+
+import KanbanBoard from './components/KanbanBoard';
+import './App.css'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <KanbanBoard />
+    </div>
+  );
+}
+
+export default App;
